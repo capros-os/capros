000145b2dd31a94f6b13f6925f4db7fcc19ae433
Charles Landau <charlie@charlielandau.com>
2007-06-19 21:56:35 +0000

    FIRST_MISC_KEYTYPE is only used for debugging.

--- src/sys/disk/KeyStruct.h
+++ src/sys/disk/KeyStruct.h
@@ -56,7 +56,9 @@ Approved for public release, distribution unlimited. */
 /* These REALLY need to get defined in StdKeyTypes, lest they drift. */
 #define LAST_GATE_KEYTYPE KKT_Resume
 #define LAST_OBJECT_KEYTYPE KKT_Page
+#ifndef NDEBUG
 #define FIRST_MISC_KEYTYPE KKT_KeyBits
+#endif
 
 typedef unsigned KeyType;
 
@@ -366,11 +368,13 @@ keyBits_NeedsPin(const KeyBits *thisPtr)
   return (keyBits_IsObjectKey(thisPtr) && !keyBits_IsGateKey(thisPtr));
 }
   
+#ifndef NDEBUG
 INLINE bool 
 keyBits_IsMiscKey(const KeyBits *thisPtr)
 {
   return (keyBits_GetType(thisPtr) >= FIRST_MISC_KEYTYPE);
 }
+#endif
 
 INLINE bool
 keyBits_IsNodeKeyType(const KeyBits *thisPtr)
