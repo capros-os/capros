f3659a0eca6f4f5cc3e3d9eb9304ed74b614ac12
Charles Landau <charlie@charlielandau.com>
2007-06-27 13:00:35 +0000

    Add test program for forwarder.

---
+++ src/base/tests/func/forwarder/proc2.c
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2007, Strawberry Development Group
+ *
+ * This file is part of the CapROS Operating System.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+/* This material is based upon work supported by the US Defense Advanced
+Research Projects Agency under Contract No. W31P4Q-07-C-0070.
+Approved for public release, distribution unlimited. */
+
+/*
+ * Proc2 -- this process reads and writes memory as instructed.
+ */
+
+#include <eros/target.h>
+#include <eros/Invoke.h>
+#include <domain/Runtime.h>
+#include <domain/domdbg.h>
+
+/* It is intended that this should be a small space domain */
+const uint32_t __rt_stack_pages = 0;
+const uint32_t __rt_stack_pointer = 0x21000;
+const uint32_t __rt_unkept = 1;	/* do not mess with keeper */
+
+#define KR_OSTREAM 10
+
+static uint8_t rcvData[EROS_PAGE_SIZE];
+
+int
+main()
+{
+  Message msg;
+  msg.snd_key0 = KR_VOID;
+  msg.snd_key1 = KR_VOID;
+  msg.snd_key2 = KR_VOID;
+  msg.snd_rsmkey = KR_VOID;
+  msg.snd_data = 0;
+  msg.snd_len = 0;
+  msg.snd_w1 = 0;
+  msg.snd_w2 = 0;
+  msg.snd_w3 = 0;
+
+  msg.rcv_key0 = KR_ARG(0);
+  msg.rcv_key1 = KR_ARG(1);
+  msg.rcv_key2 = KR_ARG(2);
+  msg.rcv_rsmkey = KR_RETURN;
+  msg.rcv_data = rcvData;
+  msg.rcv_limit = 0;
+  msg.rcv_code = 0;
+  msg.rcv_w1 = 0;
+  msg.rcv_w2 = 0;
+  msg.rcv_w3 = 0;
+
+  msg.snd_invKey = KR_VOID;
+  msg.snd_code = 0;
+  for (;;) {
+    RETURN(&msg);
+
+    msg.snd_invKey = KR_RETURN;
+    msg.snd_code = msg.rcv_code;	// echo code
+    msg.snd_w1 = msg.rcv_keyInfo;
+    msg.snd_w3 = msg.rcv_w3;	// echo w3
+    msg.snd_key2 = msg.rcv_key2;	// echo key2
+  }
+
+  return 0;
+}
