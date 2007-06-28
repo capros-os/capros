2c70863b7113bdce793b2986e6f22f1cd085138c
Charles Landau <charlie@charlielandau.com>
2007-06-28 23:03:31 +0000

    Get lat_fs working.
    Remove use of SysTrace which was machine-dependent.

--- src/base/tests/perf/lat_fs/lat_fs.c
+++ src/base/tests/perf/lat_fs/lat_fs.c
@@ -1,8 +1,8 @@
 /*
  * Copyright (C) 1998, 1999, 2001, Jonathan S. Shapiro.
- * Copyright (C) 2005, Strawberry Development Group
+ * Copyright (C) 2005, 2007, Strawberry Development Group
  *
- * This file is part of the EROS Operating System.
+ * This file is part of the CapROS Operating System.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -18,14 +18,15 @@
  * along with this program; if not, write to the Free Software
  * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
-
+/* This material is based upon work supported by the US Defense Advanced
+Research Projects Agency under Contract No. W31P4Q-07-C-0070.
+Approved for public release, distribution unlimited. */
 
 #include <eros/target.h>
 #include <eros/Invoke.h>
 #include <idl/eros/Sleep.h>
 #include <eros/ProcessKey.h>
 #include <eros/NodeKey.h>
-#include <idl/eros/SysTrace.h>
 #include <eros/KeyConst.h>
 #include <domain/domdbg.h>
 #include <domain/SpaceBankKey.h>
@@ -46,7 +47,6 @@
 #define KR_BANK     4
 #define KR_OSTREAM  5
 #define KR_SYSTRACE 6
-#define KR_SLEEP    7
 #define KR_MYSEG    8
 #define KR_NFILESRV 9
 #define KR_SCRATCH  10
@@ -55,6 +55,8 @@
 #define KR_FD       12
 #define KR_FD0      12
 #define KR_FD1      13
+
+#define KR_SLEEP    27
 
 
 #define NITER       20
@@ -65,6 +67,23 @@ const uint32_t __rt_stack_pages = 0;
 const uint32_t __rt_stack_pointer = 0x20000;
 
 uint32_t buf[EROS_MESSAGE_LIMIT/sizeof(uint32_t)];
+
+#ifdef NDEBUG
+#define assert(ignore) ((void) 0)
+#else
+
+extern int __assert(const char *, const char *, int);
+
+#define assert(expression)  \
+  ((void) ((expression) ? 0 : __assert (#expression, __FILE__, __LINE__)))
+
+int __assert(const char *expr, const char *file, int line)
+{
+  kdprintf(KR_OSTREAM, "%s:%d: Assertion failed: '%s'\n",
+	   file, line, expr);
+  return 0;
+}
+#endif
 
 void
 setup()
@@ -97,14 +116,6 @@ static int sizes[] = { 0,
 		       9 * 1024,
 #endif
 		       10 * 1024
-#if 0
-		       11 * 1024,
-		       12 * 1024,
-		       16 * 1024,
-		       17 * 1024,
-		       18 * 1024,
-		       19 * 1024,
-#endif		       20 * 1024,
 };
 
 int
@@ -113,75 +124,70 @@ main()
   uint32_t result;
   uint32_t len;
   int i, pass, file;
+  uint64_t startTime, endTime;
   
-  eros_SysTrace_info st;
-
-#if 0
-  char *addr = (char *) TEST_ADDR;
-  uint32_t sum=0;
-  
-#endif
-
   for (i = 0; i < EROS_MESSAGE_LIMIT/sizeof(uint32_t); i++)
     buf[i] = 0xdeadbeef;
   
   setup();
 
   kprintf(KR_OSTREAM, "Sleep a while\n");
-  eros_Sleep_sleep(KR_SLEEP, 4000);
+  eros_Sleep_sleep(KR_SLEEP, 2000);
   kprintf(KR_OSTREAM, "Begin tracing\n");
 
   for (i = 0; i < sizeof(sizes)/sizeof(int); i++) {
-    uint64_t cre_cycles = 0;
-    uint64_t del_cycles = 0;
-    
-    for(pass = 0; pass < NPASS; pass++) {
+    for (pass = 0; pass < NPASS; pass++) {
       DEBUG(passes)
-	kdprintf(KR_OSTREAM, "pass %d\n", pass);
+	kprintf(KR_OSTREAM, "pass %d\n", pass);
 
-      eros_SysTrace_clearKernelStats(KR_SYSTRACE);
-      eros_SysTrace_startCounter(KR_SYSTRACE, eros_SysTrace_mode_cycles);
+      result = eros_Sleep_getTimeMonotonic(KR_SLEEP, &startTime);
+      if (result != RC_OK)
+	kprintf(KR_OSTREAM, "getTime returned %d(0x%x)\n", result);
+
+      DEBUG(passes)
+	kdprintf(KR_OSTREAM, "startTime 0x%08x 0x%08x\n",
+                 (uint32_t)(startTime>>32), (uint32_t)startTime );
 
       for (file = 0; file < NITER; file++)
 	{
-	  nfile_create(KR_NFILESRV, KR_FD0);
+	  result = nfile_create(KR_NFILESRV, KR_FD0);
+          assert(result == RC_OK);
+
+          DEBUG(passes)
+            kdprintf(KR_OSTREAM, "Created nfile\n");
+
 	  result = nfile_write(KR_FD0, 0, sizes[i], buf, &len);
+          assert(result == RC_OK);
 
-	  node_swap(KR_TMPNODE, file, KR_FD0, KR_VOID);
+          DEBUG(passes)
+            kdprintf(KR_OSTREAM, "Wrote nfile\n");
+
+	  result = node_swap(KR_TMPNODE, file, KR_FD0, KR_VOID);
+          assert(result == RC_OK);
 	}
+      result = eros_Sleep_getTimeMonotonic(KR_SLEEP, &endTime);
 
-      eros_SysTrace_reportCounter(KR_SYSTRACE, &st);
+      kprintf(KR_OSTREAM, "%5dK  create: %10u us per iter\n",
+              sizes[i], (uint32_t) ((endTime - startTime)/(NITER*1000)) );
+
 #if 0
-      eros_SysTrace_stopCounter(KR_SYSTRACE);
       kdprintf(KR_OSTREAM, "Done creating\n");
 #endif
-      
-      if (cre_cycles == 0 || cre_cycles > st.cycles)
-	cre_cycles = st.cycles;
-
-      eros_SysTrace_clearKernelStats(KR_SYSTRACE);
-      eros_SysTrace_startCounter(KR_SYSTRACE, eros_SysTrace_mode_cycles);
+      result = eros_Sleep_getTimeMonotonic(KR_SLEEP, &startTime);
       for (file = 0; file < NITER; file++)
 	{
 	  node_copy(KR_TMPNODE, file, KR_FD0);
 
 	  nfile_destroy(KR_FD0);
 	}
+      result = eros_Sleep_getTimeMonotonic(KR_SLEEP, &endTime);
 
-      eros_SysTrace_reportCounter(KR_SYSTRACE, &st);
-      if (del_cycles == 0 || del_cycles > st.cycles)
-	del_cycles = st.cycles;
+      kprintf(KR_OSTREAM, "%5dK  delete: %10u us per iter\n",
+              sizes[i], (uint32_t) ((endTime - startTime)/(NITER*1000)) );
     }
-
-    kprintf(KR_OSTREAM, "%5dK  create: %10u delete: %10u   (per iter)\n",
-	    sizes[i],
-	    (uint32_t) (cre_cycles / NITER), (uint32_t)(del_cycles/NITER));
-	    
   }
   
 #if 0
-  eros_SysTrace_startCounter(KR_SYSTRACE, eros_SysTrace_mode_cycles);
-
   for (i = 0; i < NITER; i++)
   {
     nfile_create(KR_NFILESRV, KR_FD0);
@@ -208,8 +214,6 @@ main()
       kdprintf(KR_OSTREAM, "nfile_rd: result 0x%x len %d\n", result, len);
 #endif
   }
-  
-  eros_SysTrace_stopCounter(KR_SYSTRACE);
 #endif
 
   kprintf(KR_OSTREAM, "lat_fs: done %d iterations\n", NITER);
