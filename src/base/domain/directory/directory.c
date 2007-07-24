796f40b6e97a30a751cf2d366d4ab3dabe33a9b1
Charles Landau <charlie@charlielandau.com>
2007-07-24 02:16:23 +0000

    No longer need to define true and false.

--- src/base/domain/directory/directory.c
+++ src/base/domain/directory/directory.c
@@ -1,7 +1,8 @@
 /*
  * Copyright (C) 1998, 1999, Jonathan S. Shapiro.
+ * Copyright (C) 2007, Strawberry Development Group.
  *
- * This file is part of the EROS Operating System.
+ * This file is part of the CapROS Operating System.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -17,6 +18,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
+/* This material is based upon work supported by the US Defense Advanced
+Research Projects Agency under Contract No. W31P4Q-07-C-0070.
+Approved for public release, distribution unlimited. */
 
 /* Directory object.
 
@@ -141,10 +145,6 @@ struct	direct {
 #define NEXTDIR(dp) ((struct direct *) ((uint8_t *)dp + dp->d_reclen))
 
 #define PAGESTART(dp) ((struct direct *) ((uint32_t)dp & (~(EROS_PAGE_MASK))))
-
-
-#define true 1
-#define false 0
 
 
 const struct dir_template {
