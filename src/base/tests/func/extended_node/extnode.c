9c79302151c36de9d820dbf8987c887f6e48ed1d
Charles Landau <charlie@charlielandau.com>
2007-10-06 03:55:13 +0000

    Complete rewrite.

--- src/base/tests/func/extended_node/extnode.c
+++ src/base/tests/func/extended_node/extnode.c
@@ -1,85 +1,112 @@
-/* Sample small program: the obligatory ``hello world'' demo. */
+/*
+ * Copyright (C) 2007, Strawberry Development Group
+ *
+ * This file is part of the CapROS Operating System.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+/* This material is based upon work supported by the US Defense Advanced
+Research Projects Agency under Contract No. W31P4Q-07-C-0070.
+Approved for public release, distribution unlimited. */
 
-#include <eros/target.h>
-#include <eros/NodeKey.h>
-#include <eros/KeyConst.h>
+#include <eros/Invoke.h>
+#include <idl/capros/Node.h>
+#include <idl/capros/Number.h>
+#include <idl/capros/SpaceBank.h>
 #include <domain/domdbg.h>
 #include <domain/Runtime.h>
 
 #include "constituents.h"
 
 #define KR_OSTREAM KR_APP(0)
-#define KR_KEYBITS KR_APP(1)
-#define KR_TREE    KR_APP(2)
+#define KR_TREE0   KR_APP(1)
+#define KR_TREE1   KR_APP(2)
 #define KR_BIGTREE KR_APP(3)
-#define KR_SCRATCH KR_APP(4)
-#define KR_ROTREE  KR_APP(5)
+#define KR_ROTREE  KR_APP(4)
 
 int
 main(void)
 {
-  node_copy(KR_CONSTIT, KC_KEYBITS, KR_KEYBITS);
-  node_copy(KR_CONSTIT, KC_OSTREAM, KR_OSTREAM);
-  node_copy(KR_CONSTIT, KC_TREE, KR_TREE);
-  node_copy(KR_CONSTIT, KC_BIGTREE, KR_BIGTREE);
+  int i;
+  capros_Number_value numval;
 
-  kdprintf(KR_OSTREAM, "About to call node_extended_copy()\n");
+  result_t retval;
 
-  node_extended_copy(KR_BIGTREE, 0, KR_SCRATCH);
-  ShowKey(KR_OSTREAM, KR_KEYBITS, KR_SCRATCH);
+#define ckOK \
+  if (retval != RC_OK) \
+    kprintf(KR_OSTREAM, "Return code 0x%0x at line %d.\n", retval, __LINE__);
 
-  node_extended_copy(KR_BIGTREE, 1, KR_SCRATCH);
-  ShowKey(KR_OSTREAM, KR_KEYBITS, KR_SCRATCH);
+  capros_Node_getSlot(KR_CONSTIT, KC_OSTREAM, KR_OSTREAM);
+  
+  kprintf(KR_OSTREAM, "Beginning test...\n");
 
-  node_extended_copy(KR_BIGTREE, 0x20, KR_SCRATCH);
-  ShowKey(KR_OSTREAM, KR_KEYBITS, KR_SCRATCH);
+  retval = capros_SpaceBank_alloc3(KR_BANK, capros_Range_otNode
+             | (capros_Range_otNode << 8)
+             | (capros_Range_otNode << 16),
+             KR_TREE0, KR_TREE1, KR_BIGTREE);
+  ckOK
 
-  node_extended_copy(KR_BIGTREE, 0x21, KR_SCRATCH);
-  ShowKey(KR_OSTREAM, KR_KEYBITS, KR_SCRATCH);
+  // Build bigtree.
+  retval = capros_Node_setL2v(KR_BIGTREE, capros_Node_l2nSlots);
+  ckOK
+  retval = capros_Node_swapSlot(KR_BIGTREE, 0, KR_TREE0, KR_VOID);
+  ckOK
+  retval = capros_Node_swapSlot(KR_BIGTREE, 1, KR_TREE1, KR_VOID);
+  ckOK
 
-  kdprintf(KR_OSTREAM, "About to call node_extended_swap()\n");
-
-  node_extended_swap(KR_BIGTREE, 0, KR_SCRATCH, KR_SCRATCH);
-  ShowKey(KR_OSTREAM, KR_KEYBITS, KR_SCRATCH);
-
-  node_extended_swap(KR_BIGTREE, 1, KR_SCRATCH, KR_SCRATCH);
-  ShowKey(KR_OSTREAM, KR_KEYBITS, KR_SCRATCH);
-
-  node_extended_swap(KR_BIGTREE, 0x20, KR_SCRATCH, KR_SCRATCH);
-  ShowKey(KR_OSTREAM, KR_KEYBITS, KR_SCRATCH);
-
-  node_extended_swap(KR_BIGTREE, 0x21, KR_SCRATCH, KR_SCRATCH);
-  ShowKey(KR_OSTREAM, KR_KEYBITS, KR_SCRATCH);
-
-  kdprintf(KR_OSTREAM, "Values should now be rotated...\n");
-
-  node_extended_copy(KR_BIGTREE, 0, KR_SCRATCH);
-  ShowKey(KR_OSTREAM, KR_KEYBITS, KR_SCRATCH);
-
-  node_extended_copy(KR_BIGTREE, 1, KR_SCRATCH);
-  ShowKey(KR_OSTREAM, KR_KEYBITS, KR_SCRATCH);
-
-  node_extended_copy(KR_BIGTREE, 0x20, KR_SCRATCH);
-  ShowKey(KR_OSTREAM, KR_KEYBITS, KR_SCRATCH);
-
-  node_extended_copy(KR_BIGTREE, 0x21, KR_SCRATCH);
-  ShowKey(KR_OSTREAM, KR_KEYBITS, KR_SCRATCH);
-
-  kdprintf(KR_OSTREAM, "Making RO variant in key %d...\n", KR_ROTREE);
-
-  {
-    uint16_t keyData;
-    node_get_key_data(KR_BIGTREE, &keyData);
-    node_make_node_key(KR_BIGTREE, keyData | SEGMODE_RO, KR_ROTREE);
+  // Write slots 0-31 via node.
+  for (i = 0; i < 32; i++) {
+    numval.value[0] = i;
+    retval = capros_Node_writeNumber(KR_TREE0, i, numval);
+    ckOK
   }
 
-  kdprintf(KR_OSTREAM, "Following swap operation should trigger process keeper...\n");
+  // Write slots 32-63 via extended node.
+  for (; i < 64; i++) {
+    numval.value[0] = i;
+    // Use bigtree slot 31 to create a number key.
+    retval = capros_Node_writeNumber(KR_BIGTREE, 31, numval);
+    ckOK
+    retval = capros_Node_getSlot(KR_BIGTREE, 31, KR_TEMP0);	// number key
+    ckOK
 
-  node_extended_swap(KR_ROTREE, 0x0, KR_SCRATCH, KR_SCRATCH);
-  ShowKey(KR_OSTREAM, KR_KEYBITS, KR_SCRATCH);
+    retval = capros_Node_swapSlotExtended(KR_BIGTREE, i, KR_TEMP0, KR_VOID);
+    ckOK
+  }
 
+  // Read all via extended node.
+  for (i = 0; i < 64; i++) {
+    retval = capros_Node_getSlotExtended(KR_BIGTREE, i, KR_TEMP0);
+    ckOK
+    retval = capros_Number_get(KR_TEMP0, &numval.value[0],
+               &numval.value[1], &numval.value[2]);
+    ckOK
+    if (numval.value[0] != i)
+      kprintf(KR_OSTREAM, "Expecting %d got %d.\n", i, numval.value[0]);
+  }
 
-  kdprintf(KR_OSTREAM, "Test exits...\n");
+  retval = capros_Node_reduce(KR_BIGTREE, capros_Node_readOnly, KR_ROTREE);
+  ckOK
+
+  retval = capros_Node_swapSlotExtended(KR_ROTREE, 31, KR_VOID, KR_VOID);
+  if (retval != RC_capros_key_NoAccess)
+    kprintf(KR_OSTREAM, "Return code 0x%0x at line %d.\n", retval, __LINE__);
+
+  // FIXME test keeper
+
+  kprintf(KR_OSTREAM, "Done.\n");
 
   return 0;
 }
