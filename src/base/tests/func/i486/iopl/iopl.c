f88b9c804d781bcc53e433cdab3f48e4f0a1da49
Charles Landau <charlie@charlielandau.com>
2007-12-16 02:23:40 +0000

    Get test working.

--- src/base/tests/func/i486/iopl/iopl.c
+++ src/base/tests/func/i486/iopl/iopl.c
@@ -1,12 +1,14 @@
-/* Sample small program: the obligatory ``hello world'' demo. */
 
 #include <eros/target.h>
-#include <eros/i486/io.h>
+#include <eros/arch/i486/io.h>
 #include <domain/domdbg.h>
 
-#define KR_SELF     2
-#define KR_OSTREAM  3
-#define KR_BANK     4
+#define KR_OSTREAM  9
+
+/* It is intended that this should be a small space process. */
+const uint32_t __rt_stack_pages = 0;
+const uint32_t __rt_stack_pointer = 0x21000;
+const uint32_t __rt_unkept = 1; /* do not mess with keeper */
 
 int
 main(void)
@@ -15,7 +17,7 @@ main(void)
 
   /* Writing anything to port 0x80 is a no-op, so this should be
    * safe. */
-  out8(0x5, 0x80);
+  outb(0x5, 0x80);
 
   kprintf(KR_OSTREAM, "IOPL: Success (no GP fault)\n");
 
