54f9265850bb4418f81797459619e5226592cd09
Charles Landau <charlie@charlielandau.com>
2007-12-16 19:08:45 +0000

    Updates to delay.h etc.

--- src/base/lib/linux-headers/asm-arm/delay.h
+++ src/base/lib/linux-headers/asm-arm/delay.h
@@ -30,14 +30,12 @@ extern void __bad_udelay(void);
  * a constant)
  */
 extern void __udelay(unsigned long usecs);
-extern void __const_udelay(unsigned long);
 
 #define MAX_UDELAY_MS 2
 
 #define udelay(n)							\
 	(__builtin_constant_p(n) ?					\
-	  ((n) > (MAX_UDELAY_MS * 1000) ? __bad_udelay() :		\
-			__const_udelay((n) * ((2199023U*HZ)>>11))) :	\
+	  ((n) > (MAX_UDELAY_MS * 1000) ? __bad_udelay() : __udelay(n)) : \
 	  __udelay(n))
 
 #endif /* defined(_ARM_DELAY_H) */
--- src/base/lib/linux-headers/asm-i386/delay.h
+++ src/base/lib/linux-headers/asm-i386/delay.h
@@ -13,17 +13,14 @@ extern void __bad_ndelay(void);
 
 extern void __udelay(unsigned long usecs);
 extern void __ndelay(unsigned long nsecs);
-extern void __const_udelay(unsigned long usecs);
 extern void __delay(unsigned long loops);
 
-/* 0x10c7 is 2**32 / 1000000 (rounded up) */
 #define udelay(n) (__builtin_constant_p(n) ? \
-	((n) > 20000 ? __bad_udelay() : __const_udelay((n) * 0x10c7ul)) : \
+	((n) > 20000 ? __bad_udelay() : __udelay(n)) : \
 	__udelay(n))
 
-/* 0x5 is 2**32 / 1000000000 (rounded up) */
 #define ndelay(n) (__builtin_constant_p(n) ? \
-	((n) > 20000 ? __bad_ndelay() : __const_udelay((n) * 5ul)) : \
+	((n) > 20000 ? __bad_ndelay() : __ndelay(n)) : \
 	__ndelay(n))
 
 void use_tsc_delay(void);
