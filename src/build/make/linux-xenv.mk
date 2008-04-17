753da803eecc6cc565c6d201dc13b478c4f44057
Charles Landau <charlie@charlielandau.com>
2008-04-17 13:05:47 +0000

    Update for gcc 4.2.3.

--- src/build/make/linux-xenv.mk
+++ src/build/make/linux-xenv.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 1998, 1999, Jonathan S. Shapiro.
-# Copyright (C) 2005, 2006, 2007, Strawberry Development Group.
+# Copyright (C) 2005, 2006, 2007, 2008, Strawberry Development Group.
 #
 # This file is part of the EROS Operating System.
 #
@@ -38,7 +38,7 @@ EROS_RANLIB=$(EROS_XENV)/bin/$(CROSS_PREFIX)ranlib
 EROS_STRIP=$(EROS_XENV)/bin/$(CROSS_PREFIX)strip
 
 # Kludge - should not have to have version number here
-EROS_LIBGCC=$(EROS_XENV)/lib/gcc/$(EROS_CROSS_TARGET)-unknown-capros/4.2.2/libgcc.a
+EROS_LIBGCC=$(EROS_XENV)/lib/gcc/$(EROS_CROSS_TARGET)-unknown-capros/4.2.3/libgcc.a
 
 ifeq "$(EROS_TARGET)" "i486"
 EROS_GCC_KERNEL_ALIGN=-falign-functions=4
