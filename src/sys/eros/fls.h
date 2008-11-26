1a6df4f6981a86d2cf1345f134d7158e3b04402b
Charles Landau <charlie@charlielandau.com>
2008-11-26 23:02:19 +0000

    Clean up code of fls32.

--- src/sys/eros/fls.h
+++ src/sys/eros/fls.h
@@ -37,25 +37,25 @@ static inline unsigned int fls32(uint32_t x)
 
   if (!x)
     return 0;
-  if (!(x & 0xffff0000u)) {
-    x <<= 16;
-    r = 16;
+  r = 1;
+  if (x & 0xffff0000u) {
+    r += 16;
+    x >>= 16;
   }
-  else r = 32;
-  if (!(x & 0xff000000u)) {
-    x <<= 8;
-    r -= 8;
+  if (x & 0xff00u) {
+    r += 8;
+    x >>= 8;
   }
-  if (!(x & 0xf0000000u)) {
-    x <<= 4;
-    r -= 4;
+  if (x & 0xf0u) {
+    x >>= 4;
+    r += 4;
   }
-  if (!(x & 0xc0000000u)) {
-    x <<= 2;
-    r -= 2;
+  if (x & 0xcu) {
+    x >>= 2;
+    r += 2;
   }
-  if (!(x & 0x80000000u)) {
-    r -= 1;
+  if (x & 0x2u) {
+    r += 1;
   }
   return r;
 }
