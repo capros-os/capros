ca43045eb9daf498112dfb04753e665b8ec1d62c
Charles Landau <charlie@charlielandau.com>
2009-01-28 18:51:17 +0000

    Fix for 64-bit host.

--- src/base/host/bin/lsvol/dbvol.c
+++ src/base/host/bin/lsvol/dbvol.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 1998, 1999, 2001, Jonathan S. Shapiro.
- * Copyright (C) 2007, 2008, Strawberry Development Group.
+ * Copyright (C) 2007, 2008, 2009, Strawberry Development Group.
  *
  * This file is part of the CapROS Operating System.
  *
@@ -47,7 +47,7 @@ ip_cksum(uint16_t *buf, int len)
   uint16_t cksum=0;
   typedef char * caddr_t;
 
-  if ((uint32_t)buf & 0x01)
+  if ((size_t)buf & 0x01)
     {
       cksum=(*(uint8_t *)buf)<<8;
       buf=(uint16_t *)((caddr_t)buf+1);
