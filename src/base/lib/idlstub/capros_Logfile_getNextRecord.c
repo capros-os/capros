612d463c868d92dbf75356c01e6e975a2d3c2edf
Charles Landau <charlie@charlielandau.com>
2009-01-30 04:41:17 +0000

    Add IDL stubs for Logfile.

---
+++ src/base/lib/idlstub/capros_Logfile_getNextRecord.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2009, Strawberry Development Group.
+ *
+ * This file is part of the CapROS Operating System.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+/* This material is based upon work supported by the US Defense Advanced
+Research Projects Agency under Contract No. W31P4Q-07-C-0070.
+Approved for public release, distribution unlimited. */
+
+#include <eros/target.h>
+#include <eros/Invoke.h>
+#include <idl/capros/Logfile.h>
+
+result_t
+capros_Logfile_getNextRecord(cap_t _self, capros_Logfile_RecordID thisRL,
+  uint32_t maxLenToReceive, uint8_t *data, uint32_t *lengthGotten)
+{
+  Message msg = {
+    .snd_invKey = _self,
+    .snd_key0 = KR_VOID,
+    .snd_key1 = KR_VOID,
+    .snd_key2 = KR_VOID,
+    .snd_rsmkey = KR_VOID,
+    .snd_len = 0,
+    .snd_code = 2,////
+    .snd_w1 = thisRL,
+    .snd_w2 = thisRL >> 32,
+    .snd_w3 = 0,
+    .rcv_key0 = KR_VOID,
+    .rcv_key1 = KR_VOID,
+    .rcv_key2 = KR_VOID,
+    .rcv_rsmkey = KR_VOID,
+    .rcv_data = data,
+    .rcv_limit = maxLenToReceive
+  };
+
+  CALL(&msg);
+  if (msg.rcv_code == RC_OK)
+    *lengthGotten = msg.rcv_sent;
+  return msg.rcv_code;
+}
