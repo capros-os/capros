7c50c0deaf3d3c2ead85f15d6a810dcc9186d416
Charles Landau <charlie@charlielandau.com>
2009-02-14 13:11:45 +0000

    - Convert nfile to use IDL. - Remove old unused FileKey.h stuff.

---
+++ src/base/lib/idlstub/capros_File_read.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2009, Strawberry Development Group.
+ *
+ * This file is part of the CapROS Operating System runtime library.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, 59 Temple Place - Suite 330 Boston, MA 02111-1307, USA.
+ */
+/* This material is based upon work supported by the US Defense Advanced
+Research Projects Agency under Contract No. W31P4Q-07-C-0070.
+Approved for public release, distribution unlimited. */
+
+#include <eros/target.h>
+#include <eros/Invoke.h>
+#include <idl/capros/File.h>
+
+result_t
+capros_File_read(cap_t _self, uint64_t at, uint32_t length,
+  uint8_t * data, uint32_t * lengthRead)
+{
+  Message msg = {
+    .snd_invKey = _self,
+    .snd_key0 = KR_VOID,
+    .snd_key1 = KR_VOID,
+    .snd_key2 = KR_VOID,
+    .snd_rsmkey = KR_VOID,
+    .snd_len = 0,
+    .snd_code = 0,////
+    .snd_w1 = (uint32_t)at,
+    .snd_w2 = at >> 32,
+    .snd_w3 = length,
+    .rcv_key0 = KR_VOID,
+    .rcv_key1 = KR_VOID,
+    .rcv_key2 = KR_VOID,
+    .rcv_rsmkey = KR_VOID,
+    .rcv_data = data,
+    .rcv_limit = length
+  };
+
+  CALL(&msg);
+  if (msg.rcv_code == RC_OK)
+    *lengthRead = msg.rcv_sent;
+  return msg.rcv_code;
+}
