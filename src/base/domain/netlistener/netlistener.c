75b9376e98ce3d1baa9c8a686b3ce41cbc9a2e8f
Charles Landau <charlie@charlielandau.com>
2009-02-14 01:46:00 +0000

    - Clarify the interface. You construct a NetListener, then call listen() once.   stop() is not needed; use key.destroy() to stop listening.   There is no ConnectionHandler interface; a connection handler is   simply a constructor requestor.
    - Fix handling of TCPPortNum, TCPListenSocket, and TCPSocket keys.
      Allocate thread stack in BSS, eliminating malloc().
      Delete listen thread on destroy().
      Handle system restart.
      Fix other bugs.
    
    - Add dependency on libethread.a.
      Generate debugging files.

--- src/base/domain/netlistener/netlistener.c
+++ src/base/domain/netlistener/netlistener.c
@@ -28,7 +28,9 @@ Approved for public release, distribution unlimited. */
 #include <domain/ProtoSpaceDS.h>
 
 #include <idl/capros/Process.h>
+#include <idl/capros/ProcCre.h>
 #include <idl/capros/NetListener.h>
+#include <idl/capros/TCPPortNum.h>
 #include <idl/capros/TCPListenSocket.h>
 #include <idl/capros/Constructor.h>
 #include <idl/capros/Node.h>
@@ -36,36 +38,38 @@ Approved for public release, distribution unlimited. */
 #include <ethread/ethread.h>
 
 #include <domain/domdbg.h>
+#include <domain/assert.h>
 
 /* Constituent node contents */
 #include "constituents.h"
 
-/* Constants for setting up the listen process */
-#define STACK_SIZE 4096
+/* Stack for the listen process */
+unsigned long long listenStack[2048 / sizeof(unsigned long long)];
 
 /* Key registers */
-#define KR_TCP_PORTNO KR_APP(1) /* The TCP local port key - for listen */
+#define KR_TCPPortNum KR_APP(1) /* The TCP local port key - for listen */
 #define KR_CONNECTION_HANDLER_C KR_APP(2) /* The Construstor for the
 					     connection handler */
 #define KR_SOCKET     KR_APP(3) /* The socket from the listen socket */
-#define KR_LISTENER   KR_APP(4) /* The listener key or VOID if no listener */
+#define KR_LISTENPROC KR_APP(4) /* The listener process key if any */
 #define KR_HANDLER    KR_APP(5) /* The connection handler object */
-#define KR_DISCRIM    KR_APP(6) /* The Discrim key to test for listener proc */
+#define KR_TCPListenSocket KR_APP(6)
 #define KR_OSTREAM    KR_APP(9)	/* only used for debugging */
 
 /* DEBUG stuff */
 #define dbg_init	0x01u   /* debug initialization logic */
+#define dbg_errors      0x02u   /* debug errors */
 
 /* Following should be an OR of some of the above */
-#define dbg_flags   ( 0u | dbg_init )
+#define dbg_flags   ( 0u | dbg_init | dbg_errors )
 
 #define CND_DEBUG(x) (dbg_##x & dbg_flags)
 #define DEBUG(x) if (CND_DEBUG(x))
-#define DEBUG2(x,y) if (((dbg_##x|dbg_##y) & dbg_flags) == (dbg_##x|dbg_##y))
 
+bool haveListener = false;
 
 /* Internal routine prototypes */
-uint32_t listen (Message *argmsg);
+int listen(void);
 int processRequest(Message *argmsg);
 
 
@@ -76,15 +80,14 @@ main(void)
   char buff[256]; // Initial parameters
   
   capros_Node_getSlot(KR_CONSTIT, KC_OSTREAM, KR_OSTREAM); // for debug
-  capros_Node_getSlot(KR_CONSTIT, KC_DISCRIM, KR_DISCRIM); 
-  capros_Process_getKeyReg(KR_SELF, KR_VOID, KR_LISTENER); // Ensure VOID
 
-  msg.snd_invKey = KR_VOID;
-  msg.snd_key0 = KR_VOID;
+  capros_Process_makeStartKey(KR_SELF, 0, KR_TEMP0);
+
+  msg.snd_invKey = KR_RETURN;
+  msg.snd_key0 = KR_TEMP0;
   msg.snd_key1 = KR_VOID;
   msg.snd_key2 = KR_VOID;
   msg.snd_rsmkey = KR_VOID;
-  msg.snd_data = 0;
   msg.snd_len = 0;
   msg.snd_code = 0;
   msg.snd_w1 = 0;
@@ -116,7 +119,6 @@ processRequest(Message *argmsg)
 {
   uint32_t result = RC_OK;
   uint32_t code = argmsg->rcv_code;
-  Message msg;
 
   argmsg->snd_len = 0;
   argmsg->snd_w1 = 0;
@@ -130,33 +132,20 @@ processRequest(Message *argmsg)
   switch (code) {
   case OC_capros_NetListener_listen:
     {
-      result_t ret;
-      capros_Discrim_classify(KR_DISCRIM, KR_LISTENER, &ret);
-      if (capros_Discrim_clVoid != ret) {
+      if (haveListener) {
 	argmsg->snd_code = RC_capros_NetListener_Already;
 	break;
       }
 
-      capros_Process_getKeyReg(KR_SELF, KR_ARG(0), KR_TCP_PORTNO);
+      capros_Process_getKeyReg(KR_SELF, KR_ARG(0), KR_TCPPortNum);
       capros_Process_getKeyReg(KR_SELF, KR_ARG(1), KR_CONNECTION_HANDLER_C);
-      result = ethread_new_thread(KR_BANK, STACK_SIZE,
-				  (uint32_t)&listen, KR_LISTENER);
+      result = ethread_new_thread1(KR_BANK,
+                                   (uint32_t)listenStack + sizeof(listenStack),
+				   (uint32_t)&listen, KR_LISTENPROC);
       if (RC_OK == result) {
-	/* Send the listener the port and handler keys */
-	msg.snd_invKey = KR_LISTENER;
-	msg.snd_key0 = KR_TCP_PORTNO;
-	msg.snd_key1 = KR_CONNECTION_HANDLER_C;
-	msg.snd_key2 = KR_VOID;
-	msg.snd_rsmkey = KR_VOID;
-	msg.snd_data = 0;
-	msg.snd_len = 0;
-	msg.snd_code = 0;
-	msg.snd_w1 = 0;
-	msg.snd_w2 = 0;
-	msg.snd_w3 = 0;
-	SEND(&msg);
+        haveListener = true;	// true forevermore
+        ethread_start(KR_LISTENPROC);
       }
-      //      result = listen(argmsg);
       argmsg->snd_code = result;
       break;
     }
@@ -168,6 +157,12 @@ processRequest(Message *argmsg)
     }
   case OC_capros_key_destroy:
     {
+      if (haveListener) {
+        /* The listener thread is most likely waiting for a connection.
+        There needs to be a way to stop it from waiting.
+        Until then, just destroy the thread. */
+        capros_ProcCre_destroyProcess(KR_CREATOR, KR_BANK, KR_LISTENPROC);
+      }
       capros_Node_getSlot(KR_CONSTIT, KC_PROTOSPACE, KR_TEMP0);
 
       /* Invoke the protospace to destroy us and return. */
@@ -184,45 +179,45 @@ processRequest(Message *argmsg)
   return 1;
 }
 
-uint32_t
-listen(Message *argmsg)
+int
+listen(void)
 {
   Message msg;
 
-  /* Set up to receive the port and handler constructor keys */
-  msg.snd_invKey = KR_VOID;
-  msg.snd_key0 = KR_VOID;
-  msg.snd_key1 = KR_VOID;
-  msg.snd_key2 = KR_VOID;
-  msg.snd_rsmkey = KR_VOID;
-  msg.snd_data = 0;
-  msg.snd_len = 0;
-  msg.snd_code = 0;
-  msg.snd_w1 = 0;
-  msg.snd_w2 = 0;
-  msg.snd_w3 = 0;
-
-  msg.rcv_key0 = KR_TCP_PORTNO;
-  msg.rcv_key1 = KR_CONNECTION_HANDLER_C;
-  msg.rcv_key2 = KR_VOID;
-  msg.rcv_rsmkey = KR_RETURN;
-  msg.rcv_limit = 0;
-  msg.rcv_data = NULL;
-  msg.rcv_code = 0;
-  msg.rcv_w1 = 0;
-  msg.rcv_w2 = 0;
-  msg.rcv_w3 = 0;
-  RETURN(&msg);
-
-  DEBUG(init) kdprintf(KR_OSTREAM, "NetListener: waiting for connections\n");
   while (1) {
-    result_t rc = capros_TCPListenSocket_accept(KR_TCP_PORTNO, KR_SOCKET);
-    
+    DEBUG(init) kprintf(KR_OSTREAM, "NetListener: waiting for connections\n");
+    result_t rc = capros_TCPListenSocket_accept(KR_TCPListenSocket, KR_SOCKET);
+
     switch (rc) {
+    case RC_capros_key_Void:
+    case RC_capros_key_Restart:
+      // There is no TCPListenSocket cap. Get one:
+      DEBUG(init) kprintf(KR_OSTREAM, "NetListener: Getting TCPListenSocket\n");
+      rc = capros_TCPPortNum_listen(KR_TCPPortNum, KR_TCPListenSocket);
+      assert(rc == RC_OK);	// FIXME
+      break;
+
     case RC_OK:
       {
+        uint32_t ip;
+        uint16_t port;
+#if 0
+        rc = capros_key_getType(KR_SOCKET, &ip);
+        assert(rc == RC_OK);
+        assert(ip == IKT_capros_TCPSocket);
+#endif
+        rc = capros_TCPSocket_getRemoteAddr(KR_SOCKET, &ip, &port);
+        assert(rc == RC_OK);
+        DEBUG(init) kprintf(KR_OSTREAM, "Got connection from %d.%d.%d.%d:%d\n",
+                      ip >> 24,
+                      (ip >> 16) & 0xff,
+                      (ip >>  8) & 0xff,
+                      ip & 0xff,
+                      port);
+
 	rc = capros_Constructor_request(KR_CONNECTION_HANDLER_C,
 					KR_BANK, KR_SCHED, KR_SOCKET, KR_HANDLER);
+        DEBUG(init) kprintf(KR_OSTREAM, "NetListener: Built handler\n");
 	/* TODO Save handler for debugging etc. */
 	/* Send the handler off to serve the socket */
 	msg.snd_invKey = KR_HANDLER;
@@ -230,18 +225,19 @@ listen(Message *argmsg)
 	msg.snd_key1 = KR_VOID;
 	msg.snd_key2 = KR_VOID;
 	msg.snd_rsmkey = KR_VOID;
-	msg.snd_data = 0;
 	msg.snd_len = 0;
 	msg.snd_code = 0;
 	msg.snd_w1 = 0;
 	msg.snd_w2 = 0;
 	msg.snd_w3 = 0;
 	SEND(&msg);
-
-	argmsg->snd_code = rc;
       }
-    default: return 1;
+      break;
+
+    default:
+      DEBUG(errors) kdprintf(KR_OSTREAM, "NetListener: accept got rc=%#x\n",
+                             rc);
+      return 1;		// die
     }
-    return 1;
   }
 }
