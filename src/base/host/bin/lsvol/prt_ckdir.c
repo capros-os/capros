05dd52b19faecd0e70f4ecaddcb0574a74d8ac91
Charles Landau <charlie@charlielandau.com>
2009-02-20 06:05:34 +0000

    Show checkpointed processes.

--- src/base/host/bin/lsvol/prt_ckdir.c
+++ src/base/host/bin/lsvol/prt_ckdir.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 1998, 1999, Jonathan S. Shapiro.
- * Copyright (C) 2008, Strawberry Development Group.
+ * Copyright (C) 2008, 2009, Strawberry Development Group.
  *
  * This file is part of the CapROS Operating System,
  * and is derived from the EROS Operating System.
@@ -23,14 +23,41 @@
 Research Projects Agency under Contract No. W31P4Q-07-C-0070.
 Approved for public release, distribution unlimited. */
 
+#include <string.h>
+#include <disk/GenerationHdr.h>
 #include <erosimg/App.h>
 #include <erosimg/Volume.h>
 #include <disk/TagPot.h>
 #include <disk/CkptRoot.h>
 
+static void
+showProcs(uint8_t * p, unsigned int num)
+{
+  struct DiskProcessDescriptor * dpdp = (struct DiskProcessDescriptor *)p;
+  int i;
+  for (i = 0; i < num; i++, dpdp++) {
+    // dpdp is unaligned, so copy the structure:
+    struct DiskProcessDescriptor dpd;
+    memcpy(&dpd, dpdp, sizeof(struct DiskProcessDescriptor));
+
+    diag_printf("OID=%#llx ac=%#x",
+                get_target_oid(&dpd.oid), dpd.allocCount);
+    if (dpd.actHazard) {
+      diag_printf(" haz=%d\n", dpd.actHazard);
+    } else {
+      diag_printf("\n");
+    }
+  }
+}
+
 void
 PrintCkptDir(Volume* pVol)
 {
+  int i;
+  uint64_t page[EROS_PAGE_SIZE / sizeof(uint64_t)];
+  uint8_t * pagep = (uint8_t *)page;
+  int numDescrs;
+
   diag_printf("Checkpoint header:\n");
   diag_printf("  sequenceNumber        %#llx\n",
 	       pVol->curDskCkpt->mostRecentGenerationNumber);
@@ -38,7 +65,25 @@ PrintCkptDir(Volume* pVol)
 	       pVol->curDskCkpt->endLog);
   diag_printf("  maxNPAllocCount       %#x\n",
 	       pVol->curDskCkpt->maxNPAllocCount);
-		 
+
+  vol_ReadLogPage(pVol, get_target_lid(&pVol->curDskCkpt->generations[0]),
+                  page);
+
+  // Display processes.
+  DiskGenerationHdr * genHdr = (DiskGenerationHdr *)page;
+  // First the ones in the genHdr.
+  numDescrs = genHdr->processDir.nDescriptors;
+  diag_printf("%d procs in genHdr:\n", numDescrs);
+  showProcs(pagep + sizeof(DiskGenerationHdr), numDescrs);
+
+  LID dirLID = get_target_lid(&genHdr->processDir.firstDirFrame);
+  for (i = genHdr->processDir.nDirFrames; i-- > 0; dirLID++) {
+    vol_ReadLogPage(pVol, dirLID, page);
+    numDescrs = *(uint32_t *)pagep;
+    diag_printf("%d procs in dir frame %#llx:\n", numDescrs, dirLID);
+    showProcs(pagep + sizeof(uint32_t), numDescrs);
+  }
+
 #if 0	// this is not working now ...
   uint32_t thrdStart;
   uint32_t thrdEnd;
@@ -52,30 +97,12 @@ PrintCkptDir(Volume* pVol)
   dirStart = thrdEnd;
   dirEnd = dirStart + pVol->curDskCkpt->hdr.nDirPage;
 
-  diag_printf("\nCheckpoint thread pages at log locations:\n");
-  for (pg = thrdStart; pg < thrdEnd; ) {
-    for (; pg < thrdEnd; pg++)
-      diag_printf("  0x%08x", pVol->curDskCkpt->dirPage[pg]);
-    diag_printf("\n");
-  }
-
   diag_printf("\nCheckpoint directory pages at log locations:\n");
   for (pg = dirStart; pg < dirEnd; ) {
     for (; pg < dirEnd; pg++)
       diag_printf("  0x%08x", pVol->curDskCkpt->dirPage[pg]);
     diag_printf("\n");
   }
-
-  diag_printf("\nThreads:\n");
-  for (i = 0; i < vol_NumThread(pVol); i++) {
-    ThreadDirent tde = vol_GetThread(pVol, i);
-    diag_printf("OID=0x%08x%08x  ac=0x%08x  ",
-		 (uint32_t) (tde.oid>>32), (uint32_t) tde.oid, tde.allocCount);
-    if (i%2 == 1)
-      diag_printf("\n");
-  }
-  if (vol_NumThread(pVol) % 2 != 0)
-    diag_printf("\n");
 
   diag_printf("\nP/N OID                     AllocCount LogLoc\n");
   for (i = 0; i < vol_NumDirent(pVol); i++) {
