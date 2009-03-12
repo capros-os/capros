79aef37bfdd940345f1eac5a80a5dccd17f2c678
Charles Landau <charlie@charlielandau.com>
2009-03-12 19:50:50 +0000

    Change poison values to addresses that are more likely to be invalid in CapROS.

--- src/base/lib/linux-headers/linux/poison.h
+++ src/base/lib/linux-headers/linux/poison.h
@@ -7,8 +7,8 @@
  * under normal circumstances, used to verify that nobody uses
  * non-initialized list entries.
  */
-#define LIST_POISON1  ((void *) 0x00100100)
-#define LIST_POISON2  ((void *) 0x00200200)
+#define LIST_POISON1  ((void *) 0x1)
+#define LIST_POISON2  ((void *) 0x2)
 
 /********** mm/slab.c **********/
 /*
