52ad80ba931e57daff16944d88a0777c38bb820a
Charles Landau <charlie@charlielandau.com>
2009-03-29 18:57:11 +0000

    Add stubs for HTTPResource and HTTPRequestHandler.

---
+++ src/base/lib/idlstub/capros_HTTPRequestHandler_getResponseHeaderData.c
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2009, Strawberry Development Group.
+ *
+ * This file is part of the CapROS Operating System.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+/* This material is based upon work supported by the US Defense Advanced
+Research Projects Agency under Contract No. W31P4Q-07-C-0070.
+Approved for public release, distribution unlimited. */
+
+#include <eros/target.h>
+#include <eros/Invoke.h>
+#include <idl/capros/HTTPRequestHandler.h>
+
+result_t
+capros_HTTPRequestHandler_getResponseHeaderData(cap_t _self,
+  uint32_t dataLimit, uint32_t *lengthOfHeaderData, uint8_t *headerData)
+{
+  Message msg = {
+    .snd_invKey = _self,
+    .snd_key0 = KR_VOID,
+    .snd_key1 = KR_VOID,
+    .snd_key2 = KR_VOID,
+    .snd_rsmkey = KR_VOID,
+    .snd_len = 0,
+    .snd_code = 3,////
+    .snd_w1 = dataLimit,
+    .snd_w2 = 0,
+    .snd_w3 = 0,
+    .rcv_key0 = KR_VOID,
+    .rcv_key1 = KR_VOID,
+    .rcv_key2 = KR_VOID,
+    .rcv_rsmkey = KR_VOID,
+    .rcv_data = headerData,
+    .rcv_limit = dataLimit,
+  };
+
+  CALL(&msg);
+  if (msg.rcv_code == RC_OK) {
+    if (lengthOfHeaderData)
+      *lengthOfHeaderData = msg.rcv_sent;
+  }
+  return msg.rcv_code;
+}
