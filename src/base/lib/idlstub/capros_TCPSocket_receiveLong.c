a0681ff53ea0a0d8c4e3eae55e6410d1be1f1d01
Charles Landau <charlie@charlielandau.com>
2009-05-15 03:41:24 +0000

    Remove debugging code.

--- src/base/lib/idlstub/capros_TCPSocket_receiveLong.c
+++ src/base/lib/idlstub/capros_TCPSocket_receiveLong.c
@@ -24,7 +24,6 @@ Approved for public release, distribution unlimited. */
 #include <eros/target.h>
 #include <eros/Invoke.h>
 #include <idl/capros/TCPSocket.h>
-#include <domain/domdbg.h>////
 
 result_t
 capros_TCPSocket_receiveLong(uint32_t _self, uint32_t length,
@@ -41,20 +40,15 @@ capros_TCPSocket_receiveLong(uint32_t _self, uint32_t length,
     if (rqLen > capros_TCPSocket_maxReceiveLength)
       rqLen = capros_TCPSocket_maxReceiveLength;
 
-kprintf(7,"TCPSocket_receiveLong: want %d have %d receiving %d to %#x\n",
-        length, length - resid, rqLen, data);////
     uint32_t thisLengthRead;
     result = capros_TCPSocket_receive(_self,
                rqLen, &thisLengthRead, &myFlags, data);
     if (result != RC_OK) {
-kprintf(7,"TCPSocket_receiveLong: rc=%#x\n", result);////
       if (length - resid == 0)	// haven't gotten any data
         return result;
       break;		// return the data we have and ignore result.
     }
 
-kprintf(7,"TCPSocket_receiveLong: read=%d flg=%#x\n",
-  thisLengthRead, myFlags, data[0],data[1],data[thisLengthRead-2],data[thisLengthRead-1]);////
     if (thisLengthRead == 0)	// can this happen?
       break;	// couldn't read anything
     resid -= thisLengthRead;
