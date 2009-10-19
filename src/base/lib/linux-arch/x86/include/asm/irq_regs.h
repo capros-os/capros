e19e92949715122ea17dc72b01e7a5cc4dc442da
Charles Landau <charlie@charlielandau.com>
2009-10-19 17:51:12 +0000

    Adding version 2.6.30.5 headers to base/lib/linux-arch/x86/include/asm   and deleting version 2.6.22.6 headers from base/lib/linux-headers/asm-i386:   irq_regs.h   percpu.h

--- src/base/lib/linux-arch/x86/include/asm/irq_regs.h
+++ src/base/lib/linux-arch/x86/include/asm/irq_regs.h
@@ -4,16 +4,18 @@
  *
  * Jeremy Fitzhardinge <jeremy@goop.org>
  */
-#ifndef _ASM_I386_IRQ_REGS_H
-#define _ASM_I386_IRQ_REGS_H
+#ifndef _ASM_X86_IRQ_REGS_H
+#define _ASM_X86_IRQ_REGS_H
 
 #include <asm/percpu.h>
+
+#define ARCH_HAS_OWN_IRQ_REGS
 
 DECLARE_PER_CPU(struct pt_regs *, irq_regs);
 
 static inline struct pt_regs *get_irq_regs(void)
 {
-	return x86_read_percpu(irq_regs);
+	return percpu_read(irq_regs);
 }
 
 static inline struct pt_regs *set_irq_regs(struct pt_regs *new_regs)
@@ -21,9 +23,9 @@ static inline struct pt_regs *set_irq_regs(struct pt_regs *new_regs)
 	struct pt_regs *old_regs;
 
 	old_regs = get_irq_regs();
-	x86_write_percpu(irq_regs, new_regs);
+	percpu_write(irq_regs, new_regs);
 
 	return old_regs;
 }
 
-#endif /* _ASM_I386_IRQ_REGS_H */
+#endif /* _ASM_X86_IRQ_REGS_32_H */
