8764f1a201a56bfab4d2a3106e203384a46b996e
Charles Landau <charlie@charlielandau.com>
2009-10-24 17:23:43 +0000

    Upgrading headers in base/lib/linux-headers/   from version 2.6.22.6 to version 2.6.30.5:   linux/mutex.h   linux/semaphore.h

--- src/base/lib/linux-headers/linux/mutex.h
+++ src/base/lib/linux-headers/linux/mutex.h
@@ -4,14 +4,18 @@
  * started by Ingo Molnar:
  *
  *  Copyright (C) 2004, 2005, 2006 Red Hat, Inc., Ingo Molnar <mingo@redhat.com>
- * Portions Copyright (C) 2007, 2008, 2009, Strawberry Development Group.
  *
  * This file contains the main data structure and API definitions.
  */
 #ifndef __LINUX_MUTEX_H
 #define __LINUX_MUTEX_H
 
-#include <linux/semaphore.h>
+#include <linux/list.h>
+#include <linux/spinlock_types.h>
+#include <linux/linkage.h>
+#include <linux/lockdep.h>
+
+#include <asm/atomic.h>
 
 /*
  * Simple, straightforward mutexes with strict semantics:
@@ -25,7 +29,8 @@
  * - task may not exit with mutex held
  * - memory areas where held locks reside must not be freed
  * - held mutexes must not be reinitialized
- * - mutexes may not be used in irq contexts
+ * - mutexes may not be used in hardware or software interrupt
+ *   contexts such as tasklets and timers
  *
  * These semantics are fully enforced when DEBUG_MUTEXES is
  * enabled. Furthermore, besides enforcing the above rules, the mutex
@@ -40,12 +45,15 @@
  * - detects multi-task circular deadlocks and prints out all affected
  *   locks and tasks (and only those tasks)
  */
-/* A mutex does not need a wakeupsWaiting field, but for now,
-   just define it as a semaphore. */
 struct mutex {
-	struct semaphore sem;
-#ifdef CONFIG_DEBUG_MUTEXES
+	/* 1: unlocked, 0: locked, negative: locked, possible waiters */
+	atomic_t		count;
+	spinlock_t		wait_lock;
+	struct list_head	wait_list;
+#if defined(CONFIG_DEBUG_MUTEXES) || defined(CONFIG_SMP)
 	struct thread_info	*owner;
+#endif
+#ifdef CONFIG_DEBUG_MUTEXES
 	const char 		*name;
 	void			*magic;
 #endif
@@ -54,11 +62,28 @@ struct mutex {
 #endif
 };
 
+/*
+ * This is the control structure for tasks blocked on mutex,
+ * which resides on the blocked task's kernel stack:
+ */
+struct mutex_waiter {
+	struct list_head	list;
+	struct task_struct	*task;
+#ifdef CONFIG_DEBUG_MUTEXES
+	void			*magic;
+#endif
+};
+
 #ifdef CONFIG_DEBUG_MUTEXES
 # include <linux/mutex-debug.h>
 #else
 # define __DEBUG_MUTEX_INITIALIZER(lockname)
-# define mutex_init(mutex) sema_init(&(mutex)->sem, 1)
+# define mutex_init(mutex) \
+do {							\
+	static struct lock_class_key __key;		\
+							\
+	__mutex_init((mutex), #mutex, &__key);		\
+} while (0)
 # define mutex_destroy(mutex)				do { } while (0)
 #endif
 
@@ -70,10 +95,17 @@ struct mutex {
 #endif
 
 #define __MUTEX_INITIALIZER(lockname) \
-	{ .sem = __SEMAPHORE_INIT((lockname).sem, 1) }
+		{ .count = ATOMIC_INIT(1) \
+		, .wait_lock = __SPIN_LOCK_UNLOCKED(lockname.wait_lock) \
+		, .wait_list = LIST_HEAD_INIT(lockname.wait_list) \
+		__DEBUG_MUTEX_INITIALIZER(lockname) \
+		__DEP_MAP_MUTEX_INITIALIZER(lockname) }
 
 #define DEFINE_MUTEX(mutexname) \
 	struct mutex mutexname = __MUTEX_INITIALIZER(mutexname)
+
+extern void __mutex_init(struct mutex *lock, const char *name,
+			 struct lock_class_key *key);
 
 /**
  * mutex_is_locked - is the mutex locked
@@ -81,44 +113,42 @@ struct mutex {
  *
  * Returns 1 if the mutex is locked, 0 if unlocked.
  */
-static inline int fastcall mutex_is_locked(struct mutex *lock)
+static inline int mutex_is_locked(struct mutex *lock)
 {
-	return capros_atomic32_read(&lock->sem.csem.count) <= 0;
+	return atomic_read(&lock->count) != 1;
 }
 
-static inline void fastcall mutex_lock(struct mutex *lock)
-{
-  down(&lock->sem);
-}
-
-static inline int __must_check fastcall
-mutex_lock_interruptible(struct mutex *lock)
-{
-  return down_interruptible(&lock->sem);
-}
-
-
+/*
+ * See kernel/mutex.c for detailed documentation of these APIs.
+ * Also see Documentation/mutex-design.txt.
+ */
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
 extern void mutex_lock_nested(struct mutex *lock, unsigned int subclass);
 extern int __must_check mutex_lock_interruptible_nested(struct mutex *lock,
 					unsigned int subclass);
+extern int __must_check mutex_lock_killable_nested(struct mutex *lock,
+					unsigned int subclass);
+
+#define mutex_lock(lock) mutex_lock_nested(lock, 0)
+#define mutex_lock_interruptible(lock) mutex_lock_interruptible_nested(lock, 0)
+#define mutex_lock_killable(lock) mutex_lock_killable_nested(lock, 0)
 #else
+extern void mutex_lock(struct mutex *lock);
+extern int __must_check mutex_lock_interruptible(struct mutex *lock);
+extern int __must_check mutex_lock_killable(struct mutex *lock);
+
 # define mutex_lock_nested(lock, subclass) mutex_lock(lock)
 # define mutex_lock_interruptible_nested(lock, subclass) mutex_lock_interruptible(lock)
+# define mutex_lock_killable_nested(lock, subclass) mutex_lock_killable(lock)
 #endif
 
 /*
  * NOTE: mutex_trylock() follows the spin_trylock() convention,
  *       not the down_trylock() convention!
+ *
+ * Returns 1 if the mutex has been acquired successfully, and 0 on contention.
  */
-static inline int fastcall mutex_trylock(struct mutex *lock)
-{
-  return ! down_trylock(&lock->sem);
-}
-
-static inline void fastcall mutex_unlock(struct mutex *lock)
-{
-  up(&lock->sem);
-}
+extern int mutex_trylock(struct mutex *lock);
+extern void mutex_unlock(struct mutex *lock);
 
 #endif
